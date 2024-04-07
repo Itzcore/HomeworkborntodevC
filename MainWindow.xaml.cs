f9a4266f491aec3ec55e18d5b2a022e78f451a77
Itzcore <145186420+Itzcore@users.noreply.github.com>
2024-04-07 17:12:22 +0700

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Buysomething
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double i = Convert.ToDouble(income.Text);
+            double o = Convert.ToDouble(outcome.Text);
+            double h = Convert.ToDouble(hope.Text);
+
+            double u = (i - o);
+            double re = (h / u);
+            result.Text = re.ToString();
+
+        }
+
+        private void income_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void outcome_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void hope_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void result_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
